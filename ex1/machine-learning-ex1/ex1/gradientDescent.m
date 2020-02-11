a4552ddd0c6f22f66ca35f82050f2fa4a5aed71d
Sean McNamara <stmcnamara26@gmail.com>
2020-02-11 22:42:05 +0000

    Completed ex1 core activities

--- ex1/machine-learning-ex1/ex1/gradientDescent.m
+++ ex1/machine-learning-ex1/ex1/gradientDescent.m
@@ -8,22 +8,17 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
+  
+    % Calculate the hypothesis and error vectors
+    h = X * theta;
+    errors = h - y;
+    
+    % Calculate the delta_theta vector
+    delta_theta = (alpha/m) * (X' * errors);
+  
+    % Update theta
+    theta = theta - delta_theta;
 
-    % ====================== YOUR CODE HERE ======================
-    % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
-    %
-    % Hint: While debugging, it can be useful to print out the values
-    %       of the cost function (computeCost) and gradient here.
-    %
-
-
-
-
-
-
-
-    % ============================================================
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
